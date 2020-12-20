51ccae363824995955c51a04b36baf670461880f
Lunkai <lunkai.zou@intel.com>
2020-12-20 17:20:33 +0800

    Reformat code indentation.

--- aes.c
+++ aes.c
@@ -8,12 +8,12 @@
 #include "aes.h"
 #include "gmult.h"
 
-/*
- * Addition in GF(2^8)
- * http://en.wikipedia.org/wiki/Finite_field_arithmetic
- */
+ /*
+  * Addition in GF(2^8)
+  * http://en.wikipedia.org/wiki/Finite_field_arithmetic
+  */
 uint8_t gadd(uint8_t a, uint8_t b) {
-	return a^b;
+	return a ^ b;
 }
 
 /*
@@ -21,7 +21,7 @@ uint8_t gadd(uint8_t a, uint8_t b) {
  * http://en.wikipedia.org/wiki/Finite_field_arithmetic
  */
 uint8_t gsub(uint8_t a, uint8_t b) {
-	return a^b;
+	return a ^ b;
 }
 
 /*
@@ -29,7 +29,7 @@ uint8_t gsub(uint8_t a, uint8_t b) {
  * http://en.wikipedia.org/wiki/Finite_field_arithmetic
  * Irreducible polynomial m(x) = x8 + x4 + x3 + x + 1
  *
- * NOTE: This function can be easily replaced with a look up table for a speed 
+ * NOTE: This function can be easily replaced with a look up table for a speed
  *       boost, at the expense of an increase in memory size (around 65 KB). See
  *       the aes.h header file to find the macro definition.
 uint8_t gmult(uint8_t a, uint8_t b) {
@@ -43,7 +43,7 @@ uint8_t gmult(uint8_t a, uint8_t b) {
 
 		hbs = a & 0x80;
 		a <<= 1;
-		if (hbs) a ^= 0x1b; // 0000 0001 0001 1011	
+		if (hbs) a ^= 0x1b; // 0000 0001 0001 1011
 		b >>= 1;
 	}
 
@@ -57,43 +57,43 @@ uint8_t gmult(uint8_t a, uint8_t b) {
  */
 void coef_add(uint8_t a[], uint8_t b[], uint8_t d[]) {
 
-	d[0] = a[0]^b[0];
-	d[1] = a[1]^b[1];
-	d[2] = a[2]^b[2];
-	d[3] = a[3]^b[3];
+	d[0] = a[0] ^ b[0];
+	d[1] = a[1] ^ b[1];
+	d[2] = a[2] ^ b[2];
+	d[3] = a[3] ^ b[3];
 }
 
 /*
  * Multiplication of 4 byte words
  * m(x) = x4+1
  */
-void coef_mult(uint8_t *a, uint8_t *b, uint8_t *d) {
+void coef_mult(uint8_t* a, uint8_t* b, uint8_t* d) {
 
-	d[0] = gmult(a[0],b[0])^gmult(a[3],b[1])^gmult(a[2],b[2])^gmult(a[1],b[3]);
-	d[1] = gmult(a[1],b[0])^gmult(a[0],b[1])^gmult(a[3],b[2])^gmult(a[2],b[3]);
-	d[2] = gmult(a[2],b[0])^gmult(a[1],b[1])^gmult(a[0],b[2])^gmult(a[3],b[3]);
-	d[3] = gmult(a[3],b[0])^gmult(a[2],b[1])^gmult(a[1],b[2])^gmult(a[0],b[3]);
+	d[0] = gmult(a[0], b[0]) ^ gmult(a[3], b[1]) ^ gmult(a[2], b[2]) ^ gmult(a[1], b[3]);
+	d[1] = gmult(a[1], b[0]) ^ gmult(a[0], b[1]) ^ gmult(a[3], b[2]) ^ gmult(a[2], b[3]);
+	d[2] = gmult(a[2], b[0]) ^ gmult(a[1], b[1]) ^ gmult(a[0], b[2]) ^ gmult(a[3], b[3]);
+	d[3] = gmult(a[3], b[0]) ^ gmult(a[2], b[1]) ^ gmult(a[1], b[2]) ^ gmult(a[0], b[3]);
 }
 
 /*
- * The cipher Key.	
+ * The cipher Key.
  */
 int K;
 
 /*
- * Number of columns (32-bit words) comprising the State. For this 
+ * Number of columns (32-bit words) comprising the State. For this
  * standard, Nb = 4.
  */
 #define Nb  4
 
-/*
- * Number of 32-bit words comprising the Cipher Key. For this 
- * standard, Nk = 4, 6, or 8.
- */
+ /*
+  * Number of 32-bit words comprising the Cipher Key. For this
+  * standard, Nk = 4, 6, or 8.
+  */
 int Nk;
 
 /*
- * Number of rounds, which is a function of  Nk  and  Nb (which is 
+ * Number of rounds, which is a function of  Nk  and  Nb (which is
  * fixed). For this standard, Nr = 10, 12, or 14.
  */
 int Nr;
@@ -118,7 +118,7 @@ static uint8_t s_box[256] = {
 	0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a, // c
 	0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e, // d
 	0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, // e
-	0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16};// f
+	0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };// f
 
 /*
  * Inverse S-box transformation table
@@ -140,19 +140,20 @@ static uint8_t inv_s_box[256] = {
 	0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f, // c
 	0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef, // d
 	0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61, // e
-	0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d};// f
+	0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d };// f
 
 
 /*
  * Generates the round constant Rcon[i]
  */
-uint8_t R[] = {0x02, 0x00, 0x00, 0x00};
- 
-uint8_t * Rcon(uint8_t i) {
-	
+uint8_t R[] = { 0x02, 0x00, 0x00, 0x00 };
+
+uint8_t* Rcon(uint8_t i) {
+
 	if (i == 1) {
 		R[0] = 0x01; // x^(1-1) = x^0 = 1
-	} else if (i > 1) {
+	}
+	else if (i > 1) {
 		R[0] = 0x02;
 		i--;
 		while (i > 1) {
@@ -160,78 +161,78 @@ uint8_t * Rcon(uint8_t i) {
 			i--;
 		}
 	}
-	
+
 	return R;
 }
 
 /*
- * Transformation in the Cipher and Inverse Cipher in which a Round 
- * Key is added to the State using an XOR operation. The length of a 
- * Round Key equals the size of the State (i.e., for Nb = 4, the Round 
+ * Transformation in the Cipher and Inverse Cipher in which a Round
+ * Key is added to the State using an XOR operation. The length of a
+ * Round Key equals the size of the State (i.e., for Nb = 4, the Round
  * Key length equals 128 bits/16 bytes).
  */
-void add_round_key(uint8_t *state, uint8_t *w, uint8_t r) {
-	
+void add_round_key(uint8_t* state, uint8_t* w, uint8_t r) {
+
 	uint8_t c;
-	
+
 	for (c = 0; c < Nb; c++) {
-		state[Nb*0+c] = state[Nb*0+c]^w[4*Nb*r+4*c+0];   //debug, so it works for Nb !=4 
-		state[Nb*1+c] = state[Nb*1+c]^w[4*Nb*r+4*c+1];
-		state[Nb*2+c] = state[Nb*2+c]^w[4*Nb*r+4*c+2];
-		state[Nb*3+c] = state[Nb*3+c]^w[4*Nb*r+4*c+3];	
+		state[Nb * 0 + c] = state[Nb * 0 + c] ^ w[4 * Nb * r + 4 * c + 0];   //debug, so it works for Nb !=4 
+		state[Nb * 1 + c] = state[Nb * 1 + c] ^ w[4 * Nb * r + 4 * c + 1];
+		state[Nb * 2 + c] = state[Nb * 2 + c] ^ w[4 * Nb * r + 4 * c + 2];
+		state[Nb * 3 + c] = state[Nb * 3 + c] ^ w[4 * Nb * r + 4 * c + 3];
 	}
 }
 
 /*
- * Transformation in the Cipher that takes all of the columns of the 
- * State and mixes their data (independently of one another) to 
+ * Transformation in the Cipher that takes all of the columns of the
+ * State and mixes their data (independently of one another) to
  * produce new columns.
  */
-void mix_columns(uint8_t *state) {
+void mix_columns(uint8_t* state) {
 
-	uint8_t a[] = {0x02, 0x01, 0x01, 0x03}; // a(x) = {02} + {01}x + {01}x2 + {03}x3
+	uint8_t a[] = { 0x02, 0x01, 0x01, 0x03 }; // a(x) = {02} + {01}x + {01}x2 + {03}x3
 	uint8_t i, j, col[4], res[4];
 
 	for (j = 0; j < Nb; j++) {
 		for (i = 0; i < 4; i++) {
-			col[i] = state[Nb*i+j];
+			col[i] = state[Nb * i + j];
 		}
 
 		coef_mult(a, col, res);
 
 		for (i = 0; i < 4; i++) {
-			state[Nb*i+j] = res[i];
+			state[Nb * i + j] = res[i];
 		}
 	}
 }
 
 /*
- * Transformation in the Inverse Cipher that is the inverse of 
+ * Transformation in the Inverse Cipher that is the inverse of
  * MixColumns().
  */
-void inv_mix_columns(uint8_t *state) {
+void inv_mix_columns(uint8_t* state) {
 
-	uint8_t a[] = {0x0e, 0x09, 0x0d, 0x0b}; // a(x) = {0e} + {09}x + {0d}x2 + {0b}x3
+	uint8_t a[] = { 0x0e, 0x09, 0x0d, 0x0b }; // a(x) = {0e} + {09}x + {0d}x2 + {0b}x3
 	uint8_t i, j, col[4], res[4];
 
 	for (j = 0; j < Nb; j++) {
 		for (i = 0; i < 4; i++) {
-			col[i] = state[Nb*i+j];
+			col[i] = state[Nb * i + j];
 		}
 
 		coef_mult(a, col, res);
 
 		for (i = 0; i < 4; i++) {
-			state[Nb*i+j] = res[i];
+			state[Nb * i + j] = res[i];
 		}
 	}
 }
 
 /*
- * Transformation in the Cipher that processes the State by cyclically 
- * shifting the last three rows of the State by different offsets. 
+ * Transformation in the Cipher that processes the State by cyclically
+ * shifting the last three rows of the State by different offsets.
  */
-void shift_rows(uint8_t *state) {
+void shift_rows(uint8_t* state) {
 
 	uint8_t i, k, s, tmp;
 
@@ -240,36 +241,36 @@ void shift_rows(uint8_t *state) {
 		// shift(r, 4) = r;
 		s = 0;
 		while (s < i) {
-			tmp = state[Nb*i+0];
-			
+			tmp = state[Nb * i + 0];
+
 			for (k = 1; k < Nb; k++) {
-				state[Nb*i+k-1] = state[Nb*i+k];
+				state[Nb * i + k - 1] = state[Nb * i + k];
 			}
 
-			state[Nb*i+Nb-1] = tmp;
+			state[Nb * i + Nb - 1] = tmp;
 			s++;
 		}
 	}
 }
 
 /*
- * Transformation in the Inverse Cipher that is the inverse of 
+ * Transformation in the Inverse Cipher that is the inverse of
  * ShiftRows().
  */
-void inv_shift_rows(uint8_t *state) {
+void inv_shift_rows(uint8_t* state) {
 
 	uint8_t i, k, s, tmp;
 
 	for (i = 1; i < 4; i++) {
 		s = 0;
 		while (s < i) {
-			tmp = state[Nb*i+Nb-1];
-			
-			for (k = Nb-1; k > 0; k--) {
-				state[Nb*i+k] = state[Nb*i+k-1];
+			tmp = state[Nb * i + Nb - 1];
+
+			for (k = Nb - 1; k > 0; k--) {
+				state[Nb * i + k] = state[Nb * i + k - 1];
 			}
 
-			state[Nb*i+0] = tmp;
+			state[Nb * i + 0] = tmp;
 			s++;
 		}
 	}
@@ -277,44 +278,44 @@ void inv_shift_rows(uint8_t *state) {
 
 /*
  * Transformation in the Cipher that processes the State using a non­
- * linear byte substitution table (S-box) that operates on each of the 
- * State bytes independently. 
+ * linear byte substitution table (S-box) that operates on each of the
+ * State bytes independently.
  */
-void sub_bytes(uint8_t *state) {
+void sub_bytes(uint8_t* state) {
 
 	uint8_t i, j;
-	
+
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
 			// s_box row: yyyy ----
 			// s_box col: ---- xxxx
 			// s_box[16*(yyyy) + xxxx] == s_box[yyyyxxxx]
-			state[Nb*i+j] = s_box[state[Nb*i+j]];
+			state[Nb * i + j] = s_box[state[Nb * i + j]];
 		}
 	}
 }
 
 /*
- * Transformation in the Inverse Cipher that is the inverse of 
+ * Transformation in the Inverse Cipher that is the inverse of
  * SubBytes().
  */
-void inv_sub_bytes(uint8_t *state) {
+void inv_sub_bytes(uint8_t* state) {
 
 	uint8_t i, j;
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
-			state[Nb*i+j] = inv_s_box[state[Nb*i+j]];
+			state[Nb * i + j] = inv_s_box[state[Nb * i + j]];
 		}
 	}
 }
 
 /*
- * Function used in the Key Expansion routine that takes a four-byte 
- * input word and applies an S-box to each of the four bytes to 
+ * Function used in the Key Expansion routine that takes a four-byte
+ * input word and applies an S-box to each of the four bytes to
  * produce an output word.
  */
-void sub_word(uint8_t *w) {
+void sub_word(uint8_t* w) {
 
 	uint8_t i;
 
@@ -324,10 +325,10 @@ void sub_word(uint8_t *w) {
 }
 
 /*
- * Function used in the Key Expansion routine that takes a four-byte 
- * word and performs a cyclic permutation. 
+ * Function used in the Key Expansion routine that takes a four-byte
+ * word and performs a cyclic permutation.
  */
-void rot_word(uint8_t *w) {
+void rot_word(uint8_t* w) {
 
 	uint8_t tmp;
 	uint8_t i;
@@ -335,7 +336,7 @@ void rot_word(uint8_t *w) {
 	tmp = w[0];
 
 	for (i = 0; i < 3; i++) {
-		w[i] = w[i+1];
+		w[i] = w[i + 1];
 	}
 
 	w[3] = tmp;
@@ -344,41 +345,42 @@ void rot_word(uint8_t *w) {
 /*
  * Key Expansion
  */
-void aes_key_expansion(uint8_t *key, uint8_t *w) {
+void aes_key_expansion(uint8_t* key, uint8_t* w) {
 
 	uint8_t tmp[4];
 	uint8_t i;
-	uint8_t len = Nb*(Nr+1);
+	uint8_t len = Nb * (Nr + 1);
 
 	for (i = 0; i < Nk; i++) {
-		w[4*i+0] = key[4*i+0];
-		w[4*i+1] = key[4*i+1];
-		w[4*i+2] = key[4*i+2];
-		w[4*i+3] = key[4*i+3];
+		w[4 * i + 0] = key[4 * i + 0];
+		w[4 * i + 1] = key[4 * i + 1];
+		w[4 * i + 2] = key[4 * i + 2];
+		w[4 * i + 3] = key[4 * i + 3];
 	}
 
 	for (i = Nk; i < len; i++) {
-		tmp[0] = w[4*(i-1)+0];
-		tmp[1] = w[4*(i-1)+1];
-		tmp[2] = w[4*(i-1)+2];
-		tmp[3] = w[4*(i-1)+3];
+		tmp[0] = w[4 * (i - 1) + 0];
+		tmp[1] = w[4 * (i - 1) + 1];
+		tmp[2] = w[4 * (i - 1) + 2];
+		tmp[3] = w[4 * (i - 1) + 3];
 
-		if (i%Nk == 0) {
+		if (i % Nk == 0) {
 
 			rot_word(tmp);
 			sub_word(tmp);
-			coef_add(tmp, Rcon(i/Nk), tmp);
+			coef_add(tmp, Rcon(i / Nk), tmp);
 
-		} else if (Nk > 6 && i%Nk == 4) {
+		}
+		else if (Nk > 6 && i % Nk == 4) {
 
 			sub_word(tmp);
 
 		}
 
-		w[4*i+0] = w[4*(i-Nk)+0]^tmp[0];
-		w[4*i+1] = w[4*(i-Nk)+1]^tmp[1];
-		w[4*i+2] = w[4*(i-Nk)+2]^tmp[2];
-		w[4*i+3] = w[4*(i-Nk)+3]^tmp[3];
+		w[4 * i + 0] = w[4 * (i - Nk) + 0] ^ tmp[0];
+		w[4 * i + 1] = w[4 * (i - Nk) + 1] ^ tmp[1];
+		w[4 * i + 2] = w[4 * (i - Nk) + 2] ^ tmp[2];
+		w[4 * i + 3] = w[4 * (i - Nk) + 3] ^ tmp[3];
 	}
 }
 
@@ -386,29 +388,29 @@ void aes_key_expansion(uint8_t *key, uint8_t *w) {
 /*
  * Initialize AES variables and allocate memory for expanded key
  */
-uint8_t *aes_init(size_t key_size) {
+uint8_t* aes_init(size_t key_size) {
 
-        switch (key_size) {
-		default:
-		case 16: Nk = 4; Nr = 10; break;
-		case 24: Nk = 6; Nr = 12; break;
-		case 32: Nk = 8; Nr = 14; break;
+	switch (key_size) {
+	default:
+	case 16: Nk = 4; Nr = 10; break;
+	case 24: Nk = 6; Nr = 12; break;
+	case 32: Nk = 8; Nr = 14; break;
 	}
 
-	return malloc(Nb*(Nr+1)*4);
+	return malloc(Nb * (Nr + 1) * 4);
 }
 
 /*
  * Performs the AES cipher operation
  */
-void aes_cipher(uint8_t *in, uint8_t *out, uint8_t *w) {
+void aes_cipher(uint8_t* in, uint8_t* out, uint8_t* w) {
 
-	uint8_t state[4*Nb];
+	uint8_t state[4 * Nb];
 	uint8_t r, i, j;
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
-			state[Nb*i+j] = in[i+4*j];
+			state[Nb * i + j] = in[i + 4 * j];
 		}
 	}
 
@@ -427,7 +429,7 @@ void aes_cipher(uint8_t *in, uint8_t *out, uint8_t *w) {
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
-			out[i+4*j] = state[Nb*i+j];
+			out[i + 4 * j] = state[Nb * i + j];
 		}
 	}
 }
@@ -435,20 +437,20 @@ void aes_cipher(uint8_t *in, uint8_t *out, uint8_t *w) {
 /*
  * Performs the AES inverse cipher operation
  */
-void aes_inv_cipher(uint8_t *in, uint8_t *out, uint8_t *w) {
+void aes_inv_cipher(uint8_t* in, uint8_t* out, uint8_t* w) {
 
-	uint8_t state[4*Nb];
+	uint8_t state[4 * Nb];
 	uint8_t r, i, j;
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
-			state[Nb*i+j] = in[i+4*j];
+			state[Nb * i + j] = in[i + 4 * j];
 		}
 	}
 
 	add_round_key(state, w, Nr);
 
-	for (r = Nr-1; r >= 1; r--) {
+	for (r = Nr - 1; r >= 1; r--) {
 		inv_shift_rows(state);
 		inv_sub_bytes(state);
 		add_round_key(state, w, r);
@@ -461,7 +463,7 @@ void aes_inv_cipher(uint8_t *in, uint8_t *out, uint8_t *w) {
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < Nb; j++) {
-			out[i+4*j] = state[Nb*i+j];
+			out[i + 4 * j] = state[Nb * i + j];
 		}
 	}
 }
